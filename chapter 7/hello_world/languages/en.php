08ea824055321ffbdfa889c0edd1992cd71a7041
cash <cash.costello@gmail.com>
2011-07-02 18:04:56 -0400

    updated the language file formatting and comments

--- chapter 7/hello_world/languages/en.php
+++ chapter 7/hello_world/languages/en.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * English language file
+ *
+ * It is a mapping from a descriptor string to a display string.
  */
 
 $mapping = array(
-	'hello:world'			=> "Hello, World!",
-	'hello:dolly'			=> "Hello, Dolly.",
-	'hello:menu'			=> "Hello",
-	'hello:sidebar:world'	=> "Hello world",
-	'hello:sidebar:dolly'	=> "Hello dolly",
+	'hello:world'         => "Hello, World!",
+	'hello:dolly'         => "Hello, Dolly.",
+	'hello:menu'          => "Hello",
+	'hello:sidebar:world' => "Hello world",
+	'hello:sidebar:dolly' => "Hello dolly",
 
-	'hello:user'			=> "Hello, %s!",
-	'hello:blog:stats'		=> "You have posted %s blogs",
+	'hello:user'          => "Hello, %s!",
+	'hello:blog:stats'    => "You have posted %s blogs",
 );
 
+// this is the English mapping
 add_translation('en', $mapping);

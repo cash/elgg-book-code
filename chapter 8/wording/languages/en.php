4c3c6d9a2f9ef415ee07d784712755a7da664aff
cash <cash.costello@gmail.com>
2011-07-02 18:40:27 -0400

    updated the wording plugin for the latest 1.8 beta

--- chapter 8/wording/languages/en.php
+++ chapter 8/wording/languages/en.php
@@ -1,53 +1,55 @@
 <?php
-
+/**
+ * English language mapping
+ *
+ * 1. Comments -> Responses partially done
+ * 2. Blogs -> Rants
+ */
 $english = array(
-//	'generic_comments:post' => "Post a response",
-//	'generic_comments:text' => "Response",
-	'generic_comments:post' => "generic_comments:post",
-	'generic_comments:text' => "generic_comments:text",
+	// Comments -> Responses
+	'generic_comments:add' => "Leave a response",
+	'generic_comments:post' => "Post a response",
+	'generic_comments:text' => "Respond",
+	'generic_comments:latest' => "Latest responses",
 
+	
+	// Blog -> Rants
 	'blog' => 'Rants',
 	'blog:blogs' => 'Rants',
-	'blog:owned_blogs' => '%s',
-	'blog:revisions' => 'Revisions',
-	'blog:archives' => 'Archives',
 	'blog:blog' => 'Rant',
 	'item:object:blog' => 'Rants',
 
-	'blog:title:user_blogs' => '%s\'s Rants',
-	'blog:title:all_blogs' => 'All Site Rants',
-	'blog:title:friends' => 'All Friends\' Rants',
+	'blog:title:user_blogs' => '%s\'s rants',
+	'blog:title:all_blogs' => 'All site rants',
+	'blog:title:friends' => 'All friends\' rants',
+
+	'blog:group' => 'Group rant',
+	'blog:enableblog' => 'Enable group rants',
 
 	// Editing
-	'blog:new' => 'New rant',
+	'blog:add' => 'New rant',
+	'blog:write' => 'Write a rant',
 	'blog:edit' => 'Edit rant',
-	'blog:excerpt' => 'Excerpt',
-	'blog:body' => 'Body',
-	'blog:save_status' => 'Last saved: ',
-	'blog:never' => 'Never',
-	'blog:publish_date' => 'Publish Date',
-
-	// Statuses
-	'blog:status' => 'Status',
-	'blog:status:draft' => 'Draft',
-	'blog:status:published' => 'Published',
-	'blog:status:unsaved_draft' => 'Unsaved Draft',
-
-	'blog:revision' => 'Revision',
-	'blog:auto_saved_revision' => 'Auto Saved Revision',
 
 	// messages
 	'blog:message:saved' => 'Rant saved.',
 	'blog:error:cannot_save' => 'Cannot save rant.',
 	'blog:error:cannot_write_to_container' => 'Insufficient access to save rant to group.',
-	'blog:error:post_not_found' => 'This post has been removed or is invalid.',
-	'blog:messages:warning:draft' => 'There is an unsaved draft of this post!',
-	'blog:edit_revision_notice' => '(Old version)',
-	'blog:none' => 'No rants found',
+	'blog:message:deleted_post' => 'Rant deleted.',
+	'blog:error:cannot_delete_post' => 'Cannot delete rant.',
+	'blog:none' => 'No rants',
+	'blog:error:missing:title' => 'Please enter a rant title!',
+	'blog:error:missing:description' => 'Please enter the body of your rant!',
 
 	// river
-	'blog:river:create' => '%s wrote a new rant',
+	'river:create:object:blog' => '%s published a rant %s',
+	'river:comment:object:blog' => '%s commented on the rant %s',
 
+	// widget
+	'blog:widget:description' => 'Display your latest rants',
+	'blog:moreblogs' => 'More rants',
+	'blog:numbertodisplay' => 'Number of rants to display',
+	'blog:noblogs' => 'No rants',
 );
 
 add_translation("en", $english);

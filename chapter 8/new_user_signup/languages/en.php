7b7639270524de0160d68575038bac0ed4c36657
cash <cash.costello@gmail.com>
2011-07-03 14:55:40 -0400

    lesson 4 - event system

--- chapter 8/new_user_signup/languages/en.php
+++ chapter 8/new_user_signup/languages/en.php
@@ -1,9 +1,8 @@
 <?php
 
 $english = array(
-	'new_user:email' => "Admin email address",
 	'new_user:subject' => "New user signup on %s",
-	'new_user:body' => "A new user just signed up with a username of %s and an email address of %s",
+	'new_user:body' => "A new user just signed up with a name of %s and a username of %s",
 );
 
 add_translation("en", $english);
--- chapter 8/new_user_signup/start.php
+++ chapter 8/new_user_signup/start.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * New user notification
  *
@@ -9,42 +8,39 @@
  * @license http://opensource.org/licenses/gpl-2.0.php GPL 2
  */
 
-register_elgg_event_handler('init', 'system', 'new_user_init');
+elgg_register_event_handler('init', 'system', 'new_user_init');
 
 function new_user_init() {
 	// we want to be told when a new user is created
-	register_elgg_event_handler('create', 'user', 'new_user_notify');
+	elgg_register_event_handler('create', 'user', 'new_user_notify');
 }
 
 /**
  * Send email when a new user registers
  *
- * @param string $event 
- * @param string $object_type
- * @param ElggUser $user
+ * @param string   $event The event name
+ * @param string   $type  The event type
+ * @param ElggUser $user  The user that just registered
  */
-function new_user_notify($event, $object_type, $user) {
-	global $CONFIG;
+function new_user_notify($event, $type, $user) {
 
 	// do not need notification about manually created users
-	if (isadminloggedin()) {
+	if (elgg_is_admin_logged_in()) {
 		return;
 	}
 
-	// get email address from plugin settings
-	$to = get_plugin_setting('email', 'new_user_signup');
-	if (!$to) {
-		elgg_log("Email address was not set for new user signup plugin");
-		return;
-	}
+	// put my address here
+	$to = "me@example.org";
 
-	// get site email address
-	$site = get_entity($CONFIG->site_guid);
-	$site->comment_count = $site->comment_count + 1;
+	// get site email address for the from address
+	$site = elgg_get_site_entity();
 	$from = $site->email;
 
-	$subject = sprintf(elgg_echo('new_user:subject'), $CONFIG->sitename);
-	$message = sprintf(elgg_echo('new_user:body'), $user->name, $user->email);
+	$name = $user->name;
+	$username = $user->username;
+
+	$subject = elgg_echo('new_user:subject', array($site->name));
+	$message = elgg_echo('new_user:body', array($name, $username));
 
 	elgg_send_email($from, $to, $subject, $message);
 }

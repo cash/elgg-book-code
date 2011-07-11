7bacc5756b292b117d46f04150a041930d5e4a5d
cash <cash.costello@gmail.com>
2011-07-10 23:44:24 -0400

    mobile theme

---
+++ chapter 9/mobile_theme/views/mobile/css/elements/layout.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Page Layout
+ *
+ * Contains CSS for the page shell and page layout
+ *
+ * Default layout: 990px wide, centered. Used in default page shell
+ *
+ * @package Elgg.Core
+ * @subpackage UI
+ */
+?>
+
+/* ***************************************
+	PAGE LAYOUT
+*************************************** */
+/***** DEFAULT LAYOUT ******/
+
+/***** TOPBAR ******/
+.elgg-page-topbar {
+	background: #333333 url(<?php echo elgg_get_site_url(); ?>_graphics/toptoolbar_background.gif) repeat-x top left;
+	border-bottom: 1px solid #000000;
+	position: relative;
+	height: 24px;
+	z-index: 9000;
+}
+.elgg-page-topbar > .elgg-inner {
+	padding: 0 10px;
+}
+
+/***** PAGE MESSAGES ******/
+.elgg-system-messages {
+	position: fixed;
+	top: 24px;
+	right: 20px;
+	max-width: 500px;
+	z-index: 2000;
+}
+.elgg-system-messages li {
+	margin-top: 10px;
+}
+.elgg-system-messages li p {
+	margin: 0;
+}
+
+/***** PAGE HEADER ******/
+.elgg-page-header {
+	position: relative;
+	background: #4690D6 url(<?php echo elgg_get_site_url(); ?>_graphics/header_shadow.png) repeat-x bottom left;
+}
+.elgg-page-header > .elgg-inner {
+	position: relative;
+	height: 60px;
+}
+
+/***** PAGE BODY LAYOUT ******/
+.elgg-layout {
+	min-height: 360px;
+}
+.elgg-main {
+	position: relative;
+	min-height: 360px;
+	padding: 10px;
+}
+.elgg-main > .elgg-head {
+	padding-bottom: 3px;
+	border-bottom: 1px solid #CCCCCC;
+	margin-bottom: 10px;
+}
+
+/***** PAGE FOOTER ******/
+.elgg-page-footer {
+	position: relative;
+}
+.elgg-page-footer {
+	color: #999;
+}
+.elgg-page-footer a:hover {
+	color: #666;
+}
---
+++ chapter 9/mobile_theme/views/mobile/css/elements/mobile.php
@@ -0,0 +1,45 @@
+/* ***************************************
+	MOBILE FONTS
+*************************************** */
+.elgg-heading-site, .elgg-heading-site:hover {
+	font-size: 1.1em;
+	color: white;
+	font-style: italic;
+	font-family: Georgia, times, serif;
+	text-shadow: none;
+	text-decoration: none;
+}
+h2 {
+	font-size: 1.3em;
+}
+
+/* ***************************************
+	MOBILE NAVIGATION
+*************************************** */
+.elgg-menu-site {
+	overflow: hidden;
+}
+.elgg-menu-site > li > a {
+	font-weight: normal;
+	padding: 3px 3px 0px 3px;
+}
+
+#login-dropdown {
+	top: 4px;
+}
+a.elgg-button-dropdown {
+	-moz-border-radius: 0;
+	-moz-box-shadow: 0 0 0;
+	border: 0;
+	font-weight: normal;
+	font-size: 100%;
+	padding: 0px 0px;
+}
+
+.elgg-page .elgg-page-header .elgg-search {
+	bottom: 20px;
+	height: 23px;
+	position: absolute;
+	left: 2px;
+	display: none;
+}
